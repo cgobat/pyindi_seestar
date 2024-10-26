6a35ef2a82db4e61a53bc8e3ed9f6dbecc89977d
Caden Gobat <36030084+cgobat@users.noreply.github.com>
2024-10-26 03:10:30 -0600

    Install driver definitions to main drivers.xml file instead of as a separate file

--- indi/install_indi_drivers.py
+++ indi/install_indi_drivers.py
@@ -3,11 +3,12 @@
 import os
 import sys
 from pathlib import Path
+from lxml import etree as xml
 
 INDI_XML_DIR = Path("/usr/share/indi")
 INDI_BIN_DIR = Path("/usr/bin")
 SOURCE_DIR = Path(__file__).resolve().parent
-DRIVER_NAMES = ["indi_seestar"]
+DRIVER_EXE_NAME = "indi_seestar"
 
 
 def install():
@@ -22,19 +23,35 @@ def install():
     if not INDI_XML_DIR.is_dir():
         print(f"Error: directory '{INDI_XML_DIR}' does not exist. Is the INDI library installed?")
         return 1
-    xml_definition_file = SOURCE_DIR/"indi_seestar_driver_def.xml"
-    xml_destination = INDI_XML_DIR/"indi_seestar.xml"
-    xml_destination.unlink(missing_ok=True)
-    xml_destination.symlink_to(xml_definition_file)
-    print(f"- Installed driver definition XML: '{xml_destination}'")
+    xml_definition_file = INDI_XML_DIR/"drivers.xml"
+    driver_xml: xml._ElementTree = xml.parse(xml_definition_file.as_posix())
+    for group, device in zip(["Telescopes", "Focusers", "CCDs", "Filter Wheels"],
+                             ["Mount", "Focuser", "Camera", "Filter Wheel"]):
+        device_label = f"Seestar S50 {device}"
+        driver_label = f"INDI Seestar {device}"
+        devGroup: xml._Element = driver_xml.find(f"devGroup[@group='{group}']")
+        existing = devGroup.find(f"device[@label='{device_label}']")
+        if existing is None:
+            device_elem = xml.SubElement(devGroup, "device",
+                                         {"label": device_label,
+                                          "manufacturer": "ZWO"})
+            driver_elem = xml.SubElement(device_elem, "driver", {"name": driver_label})
+            driver_elem.text = DRIVER_EXE_NAME
+            version_elem = xml.SubElement(device_elem, "version")
+            version_elem.text = "0.0.1" # TODO: read from __version__ ?
+            print(f"- Added '{device_label}' driver definition to {xml_definition_file}")
+        else:
+            print(f"- Driver for '{device_label}' already exists in {xml_definition_file}")
+    xml.indent(driver_xml, space=" "*4)
+    driver_xml.write(xml_definition_file.as_posix(), encoding="UTF-8",
+                     pretty_print=True, xml_declaration=True)
 
     driver_executable = SOURCE_DIR/"indi_seestar.py"
     driver_executable.chmod(driver_executable.stat().st_mode | 0o111) # ensure executability
-    for driver_name in DRIVER_NAMES:
-        driver_destination = INDI_BIN_DIR/driver_name
-        driver_destination.unlink(missing_ok=True)
-        driver_destination.symlink_to(driver_executable)
-        print(f"- Installed driver executable: '{driver_destination}'")
+    driver_destination = INDI_BIN_DIR/DRIVER_EXE_NAME
+    driver_destination.unlink(missing_ok=True)
+    driver_destination.symlink_to(driver_executable)
+    print(f"- Installed driver executable: '{driver_destination}'")
 
     print(f"\nNOTE: modifying or removing files in the source directory ({SOURCE_DIR}) may break this installation.\n")
     return 0
